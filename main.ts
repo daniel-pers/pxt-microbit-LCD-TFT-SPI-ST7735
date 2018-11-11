4a5e4a9cbf997755b8facae01b9184ed12d827f1
daniel-pers <daniel.pers@ac-poitiers.fr>
2018-11-11 15:50:22 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -87,9 +87,9 @@ enum COLOR {
 };
 
 /**
- * TFT LCD
+ * TFT LCD SPI
  */
-//% weight=100 color=Navy icon="\uf108" block="TFT LCD SPI"
+//% weight=100 color=#0fbc11 icon="\uf108" block="TFT LCD SPI"
 namespace TFTDisplay {
     let screen_x = 0
     let screen_y = 0
@@ -376,12 +376,7 @@ namespace TFTDisplay {
             y,
             0
         )
-    }    
-	    
-
-	    
-	    
-	 
+    }         
 
     /**
     * Clear screen
